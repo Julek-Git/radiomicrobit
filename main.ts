1f8144b267761a9aaf800c053d44eb5e993afa3d
Julek-Git <84777286+Julek-Git@users.noreply.github.com>
2022-12-25 14:37:47 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+radio.onReceivedValue(function (name, value) {
+	
+})
+basic.forever(function () {
+    radio.setGroup(1)
+})
