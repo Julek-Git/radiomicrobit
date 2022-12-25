9629908aa35a91a89cef9b13c2c27788040f2da2
Julek-Git <84777286+Julek-Git@users.noreply.github.com>
2022-12-25 20:40:55 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
-	
+    basic.showString("" + num + " " + "Int: " + receivedNumber)
+    basic.pause(200)
 })
 radio.onReceivedString(function (receivedString) {
-	
+    basic.showString("" + num + " " + "Str: " + receivedString)
+    basic.pause(200)
 })
 radio.onReceivedValue(function (name, value) {
-	
+    basic.showString("" + num + " " + "Key:Val" + name + ":" + value)
+    basic.pause(200)
+    radio.sendNumber(0)
 })
+let num = 0
+num = 0
 basic.forever(function () {
-    radio.setGroup(1)
+    basic.showNumber(num)
+    radio.setGroup(num)
+    if (num == 255) {
+        num = 1
+    }
+    num += 1
+    radio.sendNumber(num)
 })
